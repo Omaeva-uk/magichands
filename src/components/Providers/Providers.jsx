d88673c4cdde3f3d446a74c581d9ae06fc4c2863
Radhakrishnan <radhakrishnanr2699@gmail.com>
2025-02-20 23:39:26 +0530

    ad width to heading

--- src/components/Providers/Providers.jsx
+++ src/components/Providers/Providers.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Providers = () => {
   return (
     <div className="my-12  xl:my-24">
-        <h2 className="font-raleway max-w-xl flex-1 font-semibold text-3xl my-24 leading-snug md:text-4xl md:leading-snug"><span className="text-red">Accredited</span> – Service Providers For</h2>
+        <h2 className="font-raleway max-w-2xl flex-1 font-semibold text-3xl my-24 leading-snug md:text-4xl md:leading-snug"><span className="text-red">Accredited</span> – Service Providers For</h2>
         <div className="grid grid-cols-5 gap-7">
             <Image src="/assets/providers/provider-1.png" width={120} height={100} alt="Accredited provider" />
             <Image src="/assets/providers/provider-2.png" width={120} height={100} alt="Accredited provider" />
